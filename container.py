5ce48e95f2ba98040373720efba63e9789fc63f4
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2025-03-25 14:03:05 -0300

    Cargar el manager.py

---
+++ container.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import tkinter as tk
+import sys
+import os
+
+class Container(tk.Frame):
+    def __init__(self, padre, controlador):
+        super().__init__(padre)
+        self.controlador = controlador
+        self.pack()
+        self.place(x=0, y=0, width=1100, height=650)
+
+    def widgets(self):
+        pass
+    
+
+
+
---
+++ manager.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from tkinter import ttk
+# from login import Login 
+# from login import Registro
+from container import Container
+import sys 
+import os
+
+
+class Manager(Tk):
+    def __init__ (self, *args, **kwagrs):
+        super().__init__(*args,**kwagrs)
+        self.title("Mini Market V1.0")
+        self.geometry("1100x650+120+20")
+        self.resizable(False, False)
+
+        container = Frame(self)
+        container.pack(side=TOP, fill=BOTH, expand=True)
+        container.configure(bg="#C6D9E3")
+
+
+
+        self.frame = {}
+        for i in (Login, Registro, Container):
+            frame = i(container, self)
+            self.frames[i] = frame
+
+        self.show_frame(Container)
+
+        self.style = ttk.Style()
+        self.style.theme_use("clam") 
+
+    def show_frame(self, container):
+        frame = self.frame[container]
+        frame.tkraise()
+
+    def main():
+        app = Manager()
+        app.mainloop()
+
+if __name__ == "__main__":
+    main():
+
+
