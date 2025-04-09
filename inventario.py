a7267be722cf58ebcfeb28ed1c2a6baa9b7b515a
Simón Cebrero <simoncebrero95@gmail.com>
2025-04-08 21:31:46 -0300

    Agregar sesión inventario + buscador

--- inventario.py
+++ inventario.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
+
 
 class Inventario(tk.Frame):
     def __init__(self, padre):
@@ -8,5 +10,35 @@ class Inventario(tk.Frame):
 
 
     def widgets(self):
-        label = Label(self,text="Inventario")
-        label.pack()
+        #================================================================================================
+        canvas_articulos = tk.LabelFrame(self, text="Articulos", font="ariald 14 bold", bg="#C6D9E3")
+        canvas_articulos.place(x=300, y=10, width=780, height=580)
+
+
+        self.canvas = tk.Canvas(canvas_articulos, bg="#C6D9E3")
+        self.scrollbar = tk.Scrollbar(canvas_articulos, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = tk.Frame(self.canvas, bg="#C6D9E3")
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+
+        self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.scrollbar.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+    #======================================================================================================
+        lblframe_buscar = LabelFrame(self, text="Buscar", font="arial 14 bold", bg="#C6D9E3")
+        lblframe_buscar.place(x=10, y=10, width=280, height=80)
+
+        self.comboboxbuscar = ttk.Combobox(lblframe_buscar,font="arial 12")
+        self.comboboxbuscar.place(x=5, y=5, width=260, height=40)
+
+    #======================================================================================================
+
+
+
